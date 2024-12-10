014678141ecfc1bad58bd3d6adf3bc5540642f03
Prasad Magar <134803909+prasadmagar1211@users.noreply.github.com>
2024-12-11 02:27:04 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+let con = document.querySelector('#container');
+    let love = document.querySelector('i');
+
+    con.addEventListener('dblclick', () => {
+        love.style.transform = 'translate(-50%, -50%) scale(1)';
+        love.style.opacity = 0.8;
+
+        setTimeout(() => {
+            love.style.opacity = 0;
+        }, 500)
+
+        setTimeout(() => {
+            love.style.transform = 'translate(-50%, -50%) scale(0)';
+        }, 500)
+    })
